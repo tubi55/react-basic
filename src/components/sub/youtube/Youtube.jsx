f6c84f723e41afd947a3a99c7f847515e9c31c10
tubi55 <tubi55@nate.com>
2023-10-05 13:37:18 +0900

    forwardRef전단계로 모달 로직 되돌림

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,7 +1,7 @@
 import Layout from '../../common/layout/Layout';
 import Modal from '../../common/modal/Modal';
 import './Youtube.scss';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 /*
 	리액트는 단방향 데이터 바인딩
 	- 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
@@ -12,11 +12,9 @@ import { useEffect, useState, useRef } from 'react';
 	- 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
 
 	useRef로 jsx는 참조 객체에 담을 수 있음
-	컴포넌트를 useRef를 통한 참조객체 담는것이 불가
 */
 
 export default function Youtube() {
-	const refEl = useRef(null);
 	const [Youtube, setYoutube] = useState([]);
 	const [IsModal, setIsModal] = useState(false);
 
@@ -44,9 +42,9 @@ export default function Youtube() {
 				{Youtube.map((data, idx) => {
 					return (
 						<article key={idx}>
-							<h2 onClick={() => console.log(refEl)}>{data.snippet.title}</h2>
+							<h2>{data.snippet.title}</h2>
 							<p>{data.snippet.description}</p>
-							<div className='pic' onClick={() => refEl.current.open()}>
+							<div className='pic' onClick={() => setIsModal(true)}>
 								<img
 									src={data.snippet.thumbnails.standard.url}
 									alt={data.title}
@@ -56,8 +54,7 @@ export default function Youtube() {
 					);
 				})}
 			</Layout>
-
-			<Modal setIsModal={setIsModal} ref={refEl}></Modal>
+			{IsModal && <Modal setIsModal={setIsModal}></Modal>}
 		</>
 	);
 }
