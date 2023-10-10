03fab5fbb2372037440195e06fa61d8e10f0ff93
tubi55 <tubi55@nate.com>
2023-10-10 09:47:58 +0900

    masonry ui 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,8 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useState, useEffect } from 'react';
+//masonry import
+import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
@@ -21,28 +23,36 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<div className='picFrame'>
-				{Pics.map((data, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<img
-									className='pic'
-									src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-								/>
-								<h2>{data.title}</h2>
-
-								<div className='profile'>
+				{/* 반복 도는 article요소를 Masonry로 wrapping후 세팅 */}
+				<Masonry
+					elementType={'div'} //Masonry컴포넌트가 변환될 태그명 지정
+					options={{ transitionDuration: '0.5s' }} //박스모션시 transition 시간 설정
+					disableImagesLoaded={false} //true이미지로딩처리 안함
+					updateOnEachImageLoad={false} //true각 이미지의 로딩처리 안함
+				>
+					{Pics.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
 									<img
-										src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
-										alt={data.owner}
+										className='pic'
+										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
 									/>
-									<span>{data.owner}</span>
+									<h2>{data.title}</h2>
+
+									<div className='profile'>
+										<img
+											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+											alt={data.owner}
+										/>
+										<span>{data.owner}</span>
+									</div>
 								</div>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
