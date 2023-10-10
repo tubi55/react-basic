44dac207bcc3cb9d3140d219405670314b6532b1
tubi55 <tubi55@nate.com>
2023-10-10 11:20:34 +0900

    검색 메서드 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -12,6 +12,7 @@ export default function Gallery() {
 		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 
 		const num = 50;
 
@@ -23,6 +24,9 @@ export default function Gallery() {
 		if (opt.type === 'user') {
 			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
 		}
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+		}
 
 		const data = await fetch(url);
 		const json = await data.json();
@@ -33,8 +37,10 @@ export default function Gallery() {
 	useEffect(() => {
 		//type: 'interest' 인터레스트 방식 갤러리 호출
 		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		fetchData({ type: 'user', id: my_id });
+		//type: 'search' 검색키워드로 갤러리 호출
+		//fetchData({ type: 'user', id: my_id });
 		//fetchData({ type: 'interest' });
+		fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
