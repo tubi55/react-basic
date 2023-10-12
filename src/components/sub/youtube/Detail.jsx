453078a9f683198f833179ad19f11aa8eb081dbe
tubi55 <tubi55@nate.com>
2023-10-12 10:50:28 +0900

    상세 페이지 데이터 출력

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -1,10 +1,11 @@
 import Layout from '../../common/layout/Layout';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Detail() {
 	//url로 전단될 parameter값을 비구조화할당으로 받을 수 있음
 	const { id } = useParams();
+	const [Data, setData] = useState(null);
 
 	useEffect(() => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
@@ -13,11 +14,19 @@ function Detail() {
 		//전체 목록이 아닌 특정 id값에 대한 유튜브 데이터 객체 하나만 받아오기 위함
 		fetch(`${baseURL}?key=${api_key}&id=${id}&part=snippet`)
 			.then((data) => data.json())
-			.then((json) => console.log(json));
+			.then((json) => {
+				console.log(json.items[0].snippet);
+				setData(json.items[0].snippet);
+			});
 	}, []);
 	return (
 		<Layout title={'Detail'}>
-			<p>유튜브 상세페이지 {id}</p>
+			<h2>{Data?.title}</h2>
+			<p>{Data?.description}</p>
+			<iframe
+				src={`https://www.youtube.com/embed/${Data?.resourceId.videoId}`}
+				title='youtube'
+			></iframe>
 		</Layout>
 	);
 }
