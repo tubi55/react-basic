576ac52d57901e715976d252e0e38dffa0e4d3fa
tubi55 <tubi55@nate.com>
2023-10-13 10:44:55 +0900

    반복도는 post에 수정버튼 클릭시 enableUpdate=true값을 추가해서 구분자 생성

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -6,6 +6,7 @@ export default function Community() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
 	const [Posts, setPosts] = useState([]);
+	console.log(Posts);
 
 	const resetForm = () => {
 		refInput.current.value = '';
@@ -27,6 +28,18 @@ export default function Community() {
 	const deletePost = (delIndex) => {
 		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
 		setPosts(Posts.filter((_, idx) => delIndex !== idx));
+	};
+
+	//해당 글을 수정모드로 변경시키는 함수
+	const enableUpdate = (editIndex) => {
+		setPosts(
+			//Posts 배열값을 반복돌면서 인수로 전달된 수정할 포스트의 순번값과 현재 반복도는 배열의 포스트 순번값이 일치하면
+			//해당 글을 수정처리해야되므로 해당 객체에 enableUpdate=true값을 추가
+			Posts.map((post, idx) => {
+				if (editIndex === idx) post.enableUpdate = true;
+				return post;
+			})
+		);
 	};
 
 	return (
@@ -57,7 +70,7 @@ export default function Community() {
 							</div>
 
 							<nav className='btnSet'>
-								<button>Edit</button>
+								<button onClick={() => enableUpdate(idx)}>Edit</button>
 								<button onClick={() => deletePost(idx)}>Delete</button>
 							</nav>
 						</article>
