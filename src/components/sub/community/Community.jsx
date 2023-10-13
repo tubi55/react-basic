766c8d3df7f33c7e856f76fb0033e462d81f7456
tubi55 <tubi55@nate.com>
2023-10-13 10:14:21 +0900

    삭제기능 구현

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -21,6 +21,12 @@ export default function Community() {
 			{ title: refInput.current.value, content: refTextarea.current.value },
 			...Posts,
 		]);
+		resetForm();
+	};
+
+	const deletePost = (delIndex) => {
+		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
+		setPosts(Posts.filter((_, idx) => delIndex !== idx));
 	};
 
 	return (
@@ -47,6 +53,11 @@ export default function Community() {
 						<article key={idx}>
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
+
+							<nav className='btnSet'>
+								<button>Edit</button>
+								<button onClick={() => deletePost(idx)}>Delete</button>
+							</nav>
 						</article>
 					);
 				})}
