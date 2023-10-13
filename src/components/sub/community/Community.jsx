b3e0c19f1e3e851e47b7169792d57714a4d92218
tubi55 <tubi55@nate.com>
2023-10-13 13:33:44 +0900

    로컬저장소에 데이터 저장

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,13 +1,21 @@
 import Layout from '../../common/layout/Layout';
 import './Community.scss';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export default function Community() {
+	//로컬데이터의 값을 parsing해서 반환하는 함수
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		if (data) return JSON.parse(data);
+		else return [];
+	};
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
 	const refEditInput = useRef(null);
 	const refEditTextarea = useRef(null);
-	const [Posts, setPosts] = useState([]);
+	//해당 컴포넌트가 처음 마운트시에는 로컬저장소에 값이 없기 때문에 빈배열 리턴
+	//저장소에 값이 있으면 해당값을 parsing된 데이터가 있는 배열값을 리턴
+	const [Posts, setPosts] = useState(getLocalData());
 	const [Allowed, setAllowed] = useState(true);
 	console.log(Posts);
 
@@ -76,6 +84,10 @@ export default function Community() {
 			})
 		);
 	};
+
+	useEffect(() => {
+		localStorage.setItem('post', JSON.stringify(Posts));
+	}, [Posts]);
 
 	return (
 		<Layout title={'Community'}>
@@ -157,4 +169,12 @@ export default function Community() {
   Delete : 게시글 삭제
 
   localStorage : 모든 브라우저가 가지고 있는 경량의 저장소 (문자열: 5MB)
+
+  로컬저장소에 데이터 저장
+  localStorage.setItem({key: 'value'}); 
+  객체를 문자화시켜서 저장
+
+  로컬저장소에 데이터 가져옴
+  localStorage.getItem(key)
+  문자화되어있는 객체를 다시 parsing해서 호출
 */
