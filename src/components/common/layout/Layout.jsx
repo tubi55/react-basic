09815eb4f09fda9d0a6d4f816bec82b45a77fba2
tubi55 <tubi55@nate.com>
2023-10-18 17:48:43 +0900

    splitText hook적용 및 layout 모션 추가

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -3,14 +3,27 @@
 */
 
 import './Layout.scss';
+import { useEffect, useRef } from 'react';
+import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ title, children }) {
+	const refFrame = useRef(null);
+	const refTitle = useRef(null);
+	const splitText = useSplitText();
+
+	useEffect(() => {
+		splitText(refTitle, 0.1);
+		setTimeout(() => {
+			refFrame.current.classList.add('on');
+		}, 300);
+	}, []);
 	return (
-		<section className={`layout ${title}`}>
+		<section ref={refFrame} className={`layout ${title}`}>
 			<figure></figure>
 
 			<div className='content'>
-				<h1>{title}</h1>
+				<h1 ref={refTitle}>{title}</h1>
+				<div className='bar'></div>
 				{children}
 			</div>
 		</section>
