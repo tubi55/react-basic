8353eead2aa8876fa5a31264aac463e53873b7cc
tubi55 <tubi55@nate.com>
2023-10-19 20:53:50 +0900

    가로 스크롤 영역 벗어날시 left 값 보정

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,35 +1,30 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import './Pics.scss';
 
 function Pics() {
 	const frame = useRef(null);
 	const box = useRef(null);
-	//const [Scroll, setScroll] = useState(0);
 
 	const handleScroll = () => {
 		const pos = frame.current.offsetTop;
-		const scroll = window.scrollY;
-		console.log('scroll', scroll);
-		const scroll2 = scroll - pos;
+		let scroll = window.scrollY;
+		let scroll2 = scroll - pos;
 
 		if (
 			scroll >= pos &&
 			scroll < pos + frame.current.clientHeight - window.innerWidth
 		) {
-			//setScroll(scroll2);
-
 			box.current.style.position = 'fixed';
 			box.current.style.left = -scroll2 + 'px';
 			box.current.style.top = '0px';
-			//box.current.style.bottom = 'auto';
 		} else if (scroll >= pos + frame.current.clientHeight - window.innerWidth) {
 			box.current.style.position = 'absolute';
 			box.current.style.top = frame.current.clientHeight - pos + 'px';
-			console.log('pos', frame.current.clientHeight - box.current.clientHeight);
-			//box.current.style.bottom = '0px';
+			box.current.style.left = -window.innerWidth * 3;
 		} else {
 			box.current.style.position = 'absolute';
-			box.current.style.top = 0;
+			box.current.style.top = '0px';
+			box.current.style.left = '0px';
 		}
 	};
 
