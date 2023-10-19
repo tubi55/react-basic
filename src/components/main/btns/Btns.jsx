361017031cda2a6fe90cbad1727b8ad25f2ee39a
tubi55 <tubi55@nate.com>
2023-10-19 14:53:45 +0900

    Btns 적용전

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,8 +1,11 @@
 import './Btns.scss';
+import { useEffect, useRef } from 'react';
 
 function Btns() {
+	const refBtns = useRef(null);
+
 	return (
-		<ul className='scroll_navi'>
+		<ul className='scroll_navi' ref={refBtns}>
 			<li className='on'></li>
 			<li></li>
 			<li></li>
