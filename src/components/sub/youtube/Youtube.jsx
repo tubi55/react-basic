d417724ac98b265c5a3b4fe276d1e3d22eda3031
tubi55 <tubi55@nate.com>
2023-10-20 16:03:47 +0900

    youtube 서브페이지 리액트 쿼리로 변경

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -2,42 +2,45 @@
 
 import Layout from '../../common/layout/Layout';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useYoutubeQuery } from '../../../hooks/useYoutube';
 import './Youtube.scss';
 
 export default function Youtube() {
-	const Youtube = useSelector((store) => store.youtube.data);
+	const { data: Youtube, isSuccess } = useYoutubeQuery();
 
 	return (
 		<>
 			<Layout title={'Youtube'}>
-				{Youtube.map((data, idx) => {
-					let tit = data.snippet.title;
-					let desc = data.snippet.description;
-					let date = data.snippet.publishedAt;
+				{isSuccess &&
+					Youtube.map((data, idx) => {
+						let tit = data.snippet.title;
+						let desc = data.snippet.description;
+						let date = data.snippet.publishedAt;
 
-					return (
-						<article key={idx}>
-							<div className='titBox'>
-								<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-							</div>
+						return (
+							<article key={idx}>
+								<div className='titBox'>
+									<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
+								</div>
 
-							<div className='conBox'>
-								<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
-								<span>{date.split('T')[0].split('-').join('.')}</span>
-							</div>
+								<div className='conBox'>
+									<p>
+										{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}
+									</p>
+									<span>{date.split('T')[0].split('-').join('.')}</span>
+								</div>
 
-							<div className='picBox'>
-								<Link to={`/detail/${data.id}`}>
-									<img
-										src={data.snippet.thumbnails.standard.url}
-										alt={data.title}
-									/>
-								</Link>
-							</div>
-						</article>
-					);
-				})}
+								<div className='picBox'>
+									<Link to={`/detail/${data.id}`}>
+										<img
+											src={data.snippet.thumbnails.standard.url}
+											alt={data.title}
+										/>
+									</Link>
+								</div>
+							</article>
+						);
+					})}
 			</Layout>
 		</>
 	);
