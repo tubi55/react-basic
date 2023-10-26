ebb3bd24db37bf150c562c67c78c40c52daf418f
tubi55 <tubi55@nate.com>
2023-10-26 20:17:25 +0900

    브라우저 리사이즈시 모바일 패널 자동 닫힘 처리

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -2,9 +2,24 @@ import './Menu.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useGlobalData } from '../../../hooks/useGlobalContext';
+import { useEffect } from 'react';
 
 function Menu() {
 	const { MenuOpen, setMenuOpen } = useGlobalData();
+
+	useEffect(() => {
+		//브라우저가 1000px이상 폭이 될때 setMeunuOpen(false) 로 메뉴를 닫는 핸들러 함수 정의
+		const closeMenu = () => {
+			const wid = window.innerWidth;
+			if (wid >= 1000) setMenuOpen(false);
+		};
+
+		//윈도우 전역 객체에 resize 이벤트 핸들러 연결
+		window.addEventListener('resize', closeMenu);
+		//해당 컴포넌트 언마운트시 이벤트 핸들러 제거
+		return () => window.removeEventListener('resize', closeMenu);
+	}, [setMenuOpen]);
+
 	return (
 		<AnimatePresence>
 			{MenuOpen && (
@@ -59,3 +74,10 @@ function Menu() {
 }
 
 export default Menu;
+
+/*
+모바일 화면에서 모바일 메뉴 패널이 열려있는 상태에서 브라우저를 데스크탑 버전으로 리사이즈시 모바일 패널 그대로 남아있는 현상 해결
+- 원인 모바일 패널은 햄버거 버튼이나 모바일 패널을 클릭해야지만 닫히도록 되어 있는데 모바일 패널이 열려있는 상태에서 리사이즈해서 늘리면 버튼이 사라지기 때문에 닫히지 않음
+- 해결 방법
+- 모바일 패널 컴포넌트에서 윈도우 객체에 리사이즈 이벤트를 연결해서 브라우저가 1000px이상 들어나면 자동으로 패널 닫히도록 처리
+*/
